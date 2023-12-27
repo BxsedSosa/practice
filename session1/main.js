ab486262fb5425798dee2a9330447b02f3006ef4
Donovan <donovanxabad@gmail.com>
2023-12-27 11:20:07 -0800

    Update main.js
    Adding comments to explain what the function is doing

--- session1/main.js
+++ session1/main.js
@@ -1,6 +1,8 @@
 //Using a for loop print all even numbers up to and including n. Dont include 0
 
 function evenNum (num) {
+  
+  //starts at 2 and only adds 2
   for (i=2;i <= num;i+=2) {
       console.log(i);
   }
@@ -13,6 +15,7 @@ console.log('-------------------------');
 //Print sum of a number from 1 to n
 
 function printSum (num) {
+  //prints as many numebers that were set in the function
   for (i=1;i <= num; i++) {
       console.log(i);
   }
@@ -25,9 +28,10 @@ console.log('-------------------------');
 
 function evenOdd (num) {
   for (i=0;i <= num; i++) {
+    //Uses remainder operator to determine if the number is divisble by 2
       if (i % 2 === 0) {
           console.log(`${i} is even`);
-      } else {
+      } else { //Spits out everything else to odd
           console.log(`${i} is odd`);
       }
       }
@@ -42,11 +46,12 @@ console.log('-------------------------');
 
 function fizzBuzz (num) {
   for (i=1;i <= num; i++) {
+    //if the number is divisible by 3 AND 5 then logs FizzBuzz
       if (i % 3 === 0 && i % 5 === 0) {
           console.log(`${i} = FizzBuzz`);
-      } else if (i % 5 === 0) {
+      } else if (i % 5 === 0) { //if the number is divisible by 5 prints Buzz
           console.log(`${i} = Buzz`);
-      } else if (i % 3 === 0) {
+      } else if (i % 3 === 0) { //if the number is divisible by 3 prints Fizz
           console.log(`${i} = Fizz`);
       }
   }
